3c6244502478f086b3b1ad8f508fb223260ca26a
karolinneczka <okaa93@gmail.com>
2018-11-22 19:35:31 +0100

    Add some functions and eventlisteners

--- js/script.js
+++ js/script.js
@@ -20,10 +20,97 @@ const strictButton = document.querySelector("#strict");
 const onButton = document.querySelector("#on");
 const startButton = document.querySelector("#start");
 
+const darkgreen = "rgb(0, 107, 62)";
+const lightgreen = "rgb(0, 233, 136)";
+const darkred = "rgb(151, 0, 33)";
+const lightred = "rgb(255, 0, 55)";
+const darkyellow = "rgb(133, 113, 1)";
+const lightyellow = "rgb(255, 217, 0)";
+const darkblue = "rgb(0, 59, 107)";
+const lightblue = "rgb(0, 138, 252)";
+
 strictButton.addEventListener("click", (event) => { //change event will be also good
     if (strictButton.checked) {
         strict = true;
     } else {
         strict = false;
     }
-})
+});
+
+onButton.addEventListener('click', (event) => {
+    if (onButton.checked) {
+        on = true;
+        turnCounter.innerHTML = "0";
+    } else {
+        on = false;
+        turnCounter.innerHTML = "";
+        clearColor(); //all the lights should go off
+        clearInterval(intervalId); //!!!! explain later !!!!
+    }
+});
+
+startButton.addEventListener("click", (event) => {
+    if (on || win) { //if on or win is true
+        play();
+    }
+});
+
+function play() { //the state at the start of the game
+    win = false;
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    intervalId = 0;
+    turn = 1;
+    turnCounter.innerHTML = 1;
+    good = true; //player did nothing incorrect yet
+    for (var i = 0; i < 20; i++) { //20 points is max
+        order.push(Math.floor(Math.random() * 4) + 1) //without plus 1 it would be numbers from 0 to 3
+    }
+    compTurn = true; //computer shows colors at first
+    intervalId = setInterval(gameTurn, 800); //run the gameTurn fuction every 800 ms (colors)
+}
+
+function gameTurn () {
+    on = false; //because player cannot click buttons while computer turn is
+    if (flash === turn) { //then computer turn is over
+        clearInterval(intervalId);
+        compTurn = false;
+        clearColor();
+        on = true; // then player can start pressing buttons
+    }
+
+    if (compTurn) {
+        clearColor();
+        setTimeout(() => {
+            if (order[flash] === 1) one();
+            if (order[flash] === 2) two();
+            if (order[flash] === 3) three();
+            if (order[flash] === 4) four();
+            flash++;
+        }, 200); // do after 200 ms
+    }
+}
+
+function one() {
+    topLeft.style.backgroundColor = lightgreen;
+}
+
+function two() {
+    topRight.style.backgroundColor = lightred;
+}
+
+function three() {
+    bottomLeft.style.backgroundColor = lightyellow;
+}
+
+function four() {
+    bottomRight.style.backgroundColor = lightblue;
+}
+
+function clearColor() {
+    topLeft.style.backgroundColor = darkgreen;
+    topRight.style.backgroundColor = darkred;
+    bottomLeft.style.backgroundColor = darkyellow;
+    bottomRight.style.backgroundColor = darkblue;
+}
