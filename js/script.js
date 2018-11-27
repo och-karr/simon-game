382aff329d03988be335b2b175b6bb8f51f6344f
karolinneczka <okaa93@gmail.com>
2018-11-27 14:59:51 +0100

    Add some logic

--- js/script.js
+++ js/script.js
@@ -114,3 +114,103 @@ function clearColor() {
     bottomLeft.style.backgroundColor = darkyellow;
     bottomRight.style.backgroundColor = darkblue;
 }
+
+function flashColor() {
+    topLeft.style.backgroundColor = lightgreen;
+    topRight.style.backgroundColor = lightred;
+    bottomLeft.style.backgroundColor = lightyellow;
+    bottomRight.style.backgroundColor = lightblue;
+}
+
+topLeft.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(1); //if player click topLeft then push '1' into playerOrder array
+        check();
+        one();
+        if(!win) {
+            setTimeout(() => {
+                clearColor();
+            }, 300);
+        }
+    }
+})
+
+topRight.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(2); //if player click topLeft then push '1' into playerOrder array
+        check();
+        two();
+        if(!win) {
+            setTimeout(() => {
+                clearColor();
+            }, 300);
+        }
+    }
+})
+
+bottomLeft.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(3); //if player click topLeft then push '1' into playerOrder array
+        check();
+        three();
+        if(!win) {
+            setTimeout(() => {
+                clearColor();
+            }, 300);
+        }
+    }
+})
+
+bottomRight.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(4); //if player click topLeft then push '1' into playerOrder array
+        check();
+        four();
+        if(!win) {
+            setTimeout(() => {
+                clearColor();
+            }, 300);
+        }
+    }
+})
+
+function check() {
+    if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) 
+        good = false;
+    if (playerOrder.length === 20 && good) {
+        winGame();
+    }
+    if (good === false) {
+        flashColor();
+        turnCounter.innerHTML = "NO!";
+        setTimeout(() => {
+            turnCounter.innerHTML = turn;
+            clearColor();
+            if (strict) {
+                play();
+            } else {
+                compTurn = true;
+                flash = 0;
+                playerOrder = [];
+                good = true;
+                intervalId = setInterval(gameTurn, 800);
+            }
+        }, 800);
+        // noise = false;
+    }
+    if (turn === playerOrder.length && good && !win) {
+        turn++;
+        playerOrder = [];
+        compTurn = true;
+        flash = 0;
+        turnCounter.innerHTML = turn;
+        intervalId = setInterval(gameTurn, 800);
+    }
+}
+
+function winGame() {
+    flashColor();
+    turnCounter.innerHTML = "WIN!";
+    on = false;
+    win = true;
+}
